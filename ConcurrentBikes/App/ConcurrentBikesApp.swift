bef2bae402587fb4ae93781e28848599f89be6e5
Alberto Caamaño Souto <albertocsouto@gmail.com>
2021-12-15 22:41:43 +0100

    Using XCode 13.2 to lower to iOS 13 target. Closes #1

--- ConcurrentBikes/App/ConcurrentBikesApp.swift
+++ ConcurrentBikes/App/ConcurrentBikesApp.swift
@@ -5,13 +5,12 @@
 //  Created by Sergio Rodríguez Rama on 17/7/21.
 //
 
-import SwiftUI
+import UIKit
 
 @main
-struct ConcurrentBikesApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Hello world")
+        return true
     }
 }
